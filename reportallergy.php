0c44c60f2b2c806b33b9d1691a32c4a027cb03d7
alice2382 <aliyahtemam@gmail.com>
2023-06-11 16:55:30 +0300

    report allergy

--- reportallergy.php
+++ reportallergy.php
@@ -66,7 +66,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Report allergy</title>
+    <title>Contact</title>
     <!-- Add the Bootstrap CSS file -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
@@ -86,26 +86,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <?php @include 'header.php'; ?>
         <section class="contact">
             <h1 class="heading">Report Allergy</h1>
-            <form action="" method="post">
-                <div class="flex">
-                    <div class="inputBox">
-                        <span>Symptom:</span>
-                        <span class="error" style="color: red;"><?php echo $symptomErr; ?></span>
-                        <input type="checkbox" name="rash" value="1"> Rash
-                        <input type="checkbox" name="fever" value="1"> Fever
-                        <input type="checkbox" name="vomit" value="1"> Vomit
+            <form action="" method="post" class="form">
+                <div class="form-group">
+                    <label for="symptoms" class="label">Symptoms:</label>
+                    <span class="error"><?php echo $symptomErr; ?></span>
+                    <div class="checkbox-group">
+                        <label class="checkbox-label"><input type="checkbox" name="rash" value="1">Rash </label>
+                        <label class="checkbox-label"><input type="checkbox" name="fever" value="1"> Fever</label>
+                        <label class="checkbox-label"><input type="checkbox" name="vomit" value="1">Vomit </label>
                     </div>
-                    <div class="inputBox">
-                        <span>Date of vaccination</span>
-                        <span class="error" style="color: red;"><?php echo $dateErr; ?></span>
-                        <input type="date" name="date" min="2022-01-01" max="2050-12-31">
-                    </div>
-                    <input type="submit" value="Send" name="send" id="send" class="btn">
                 </div>
+                <div class="form-group">
+                    <label for="date" class="label">Date of vaccination:</label>
+                    <span class="error"><?php echo $dateErr; ?></span>
+                    <input type="date" name="date" min="2022-01-01" max="2050-12-31" class="input">
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Report" name="send" id="send" class="btn">
+                </div>
+            </form>
 
 
 
 
+            <form>
                 <!-- Modal -->
                 <div class="modal fade" id="myModal" role="dialog">
                     <div class="modal-dialog">
