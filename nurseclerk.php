8d6d777ab121c29ad5416856d18df515156e6073
Hiwotde <hiwotdegamlak@gmail.com>
2023-06-11 15:41:04 +0300

    Post general information

--- nurseclerk.php
+++ nurseclerk.php
@@ -129,7 +129,7 @@
       <a href="schedule.php"><i class="fas fa-calendar-alt"></i> Schedule</a>
       <a href="vaccinemanagement.php"><i class="fas fa-syringe"></i> Manage Vaccine</a>
       <a href="seeallergyreport.php"><i class="fas fa-notes-medical"></i> See Allergy Report</a>
-      <a href="content.php"><i class="fas fa-plus-circle"></i> Add Content</a>
+      <a href="content.php"><i class="fas fa-plus-circle"></i> Post General Information</a>
     </div>
   </div>
 
