331d665b0075531b1290870d7579b01200d95be3
alice2382 <aliyahtemam@gmail.com>
2023-06-15 11:54:09 +0300

    parent.php

--- parentnavigation.php
+++ parentnavigation.php
@@ -1,4 +1,4 @@
 <div class="box">
-            <a href="seeschedule.php"> <i class="fas fa-angle-right"></i> see schedule</a>
-            <a href="reportallergy.php"> <i class="fas fa-angle-right"></i> report allergy</a>
+            <a href="seeschedule.php"> <i class="fas fa-calendar-alt"></i> see schedule</a>
+            <a href="reportallergy.php"> <i class="fas fa-exclamation-triangle"></i> report allergy</a>
 </div>
