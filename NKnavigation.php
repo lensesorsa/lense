835225f33ba642ef26599e8c972ee073359449be
Lense Sorsa <sorsalense@gmail.com>
2023-06-18 21:09:14 +0300

    delete session

--- NKnavigation.php
+++ NKnavigation.php
@@ -1,10 +1,4 @@
 
-<?php 
-  session_start();
-  if (!isset($_SESSION['user_id']) || !isset($_SESSION['name']) || $_SESSION['role'] !== 'nurseclerk') {
-   header("location:home.php");
-   
-}
 ?>
 <a href="register.php"><i class="fas fa-user-plus"></i> Register</a>
             <a href="schedule.php"><i class="fas fa-calendar-alt"></i> Schedule</a>
