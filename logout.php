48c86cc79bd332740431856934c927b04de10ac9
Lense Sorsa <sorsalense@gmail.com>
2023-06-18 21:09:50 +0300

    destroy session

--- logout.php
+++ logout.php
@@ -4,6 +4,7 @@ unset($_SESSION["p_id"]);
 unset($_SESSION["c_id"]);
 unset($_SESSION["name"]);
 unset($_SESSION["password"]);
+session_destroy();
 header("Location:home.php");
 ?>
 <!-- correct  -->
